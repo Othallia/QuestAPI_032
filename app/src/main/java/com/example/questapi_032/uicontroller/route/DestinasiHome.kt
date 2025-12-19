a1d8748c006f08fe08c4ed5217a803d2fdcfb6fe
Othallia <vianiputriocta6@gmail.com>
2025-12-19 19:53:17 +0700

    menambahkan route navigasi home

---
+++ app/src/main/java/com/example/questapi_032/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.questapi_032.uicontroller.route
+
+import com.example.questapi_032.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
