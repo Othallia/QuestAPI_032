2cc3e2a62b28c901a876693ceceafac9121cb802
Othallia <vianiputriocta6@gmail.com>
2025-12-19 20:02:21 +0700

    menambahkan HomeViewModel dan status ui siswa

---
+++ app/src/main/java/com/example/questapi_032/uicontroller/viewmodel/HomeViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.questapi_032.uicontroller.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi_032.modeldata.DataSiswa
+import com.example.questapi_032.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+sealed interface StatusUiSiswa {
+    data class Success(val siswa: List<DataSiswa>) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
+
+class HomeViewModel(private val repositoryDataSiswa: RepositoryDataSiswa) : ViewModel() {
+
+    var listSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+
+    init {
+        loadSiswa()
+    }
+
+    fun loadSiswa() {
+        viewModelScope.launch {
+            listSiswa = StatusUiSiswa.Loading
+            listSiswa = try {
+                // Saya sesuaikan jadi getSiswa() agar cocok dengan Repository kamu
+                StatusUiSiswa.Success(repositoryDataSiswa.getSiswa())
+            } catch (e: IOException) {
+                StatusUiSiswa.Error
+            } catch (e: HttpException) {
+                StatusUiSiswa.Error
+            }
+        }
+    }
+}
