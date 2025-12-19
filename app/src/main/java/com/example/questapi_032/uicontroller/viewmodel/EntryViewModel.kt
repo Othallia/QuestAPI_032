6f90d68658b386a131bee1e969387f3e2824d386
Othallia <vianiputriocta6@gmail.com>
2025-12-19 19:54:18 +0700

    menambahkan EntryViewModel dan validasi input

---
+++ app/src/main/java/com/example/questapi_032/uicontroller/viewmodel/EntryViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.questapi_032.uicontroller.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.questapi_032.modeldata.DetailSiswa
+import com.example.questapi_032.modeldata.UIStateSiswa
+import com.example.questapi_032.modeldata.toDataSiswa
+import com.example.questapi_032.repositori.RepositoryDataSiswa
+
+class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    /* Fungsi untuk memvalidasi input */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
