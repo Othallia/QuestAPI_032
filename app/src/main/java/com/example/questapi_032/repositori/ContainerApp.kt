7401b92823d6c17ec3f4cf3d4408579c5e1ebe5d
Othallia <vianiputriocta6@gmail.com>
2025-12-19 19:50:33 +0700

    menambahkan container aplikasi dan konfigurasi retrofit

---
+++ app/src/main/java/com/example/questapi_032/repositori/ContainerApp.kt
@@ -0,0 +1,52 @@
+package com.example.questapi_032.repositori
+
+import android.app.Application
+import com.example.questapi_032.service_api.ServiceApiSiswa
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
+// Container untuk Injeksi Ketergantungan
+interface ContainerApp {
+    val repositoryDataSiswa: RepositoryDataSiswa
+}
+
+class DefaultContainerApp : ContainerApp {
+    // IP Address Laptop kamu (sudah benar)
+    val baseUrl = "http://192.168.0.114/umyTI/"
+
+    private val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val klien = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(
+            Json {
+                ignoreUnknownKeys = true
+                prettyPrint = true
+                isLenient = true
+            }.asConverterFactory("application/json".toMediaType())
+        )
+        // PERBAIKAN DI SINI: pakai 'baseUrl' (U besar) sesuai variabel di atas
+        .baseUrl(baseUrl)
+        .client(klien)
+        .build()
+
+    // Service API
+    private val retrofitService: ServiceApiSiswa by lazy {
+        retrofit.create(ServiceApiSiswa::class.java)
+    }
+
+    // Repository
+    override val repositoryDataSiswa: RepositoryDataSiswa by lazy {
+        JaringanRepositoryDataSiswa(serviceApiSiswa = retrofitService)
+    }
+}
+
