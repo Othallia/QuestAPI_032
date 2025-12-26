87b55fa13d212882549d98f901ed0dc17a93d5e3
Othallia <vianiputriocta6@gmail.com>
2025-12-26 13:57:16 +0700

    menambahkan interface RepositoryDataSiswa

--- app/src/main/java/com/example/questapi_032/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_032/repositori/RepositoryDataSiswa.kt
@@ -2,52 +2,13 @@ package com.example.questapi_032.repositori
 
 import com.example.questapi_032.apiservice.ServiceApiSiswa
 import com.example.questapi_032.modeldata.DataSiswa
-import java.io.IOException
+import retrofit2.Response
 
-// 1. INTERFACE (Kontrak Menu)
 interface RepositoryDataSiswa {
-    suspend fun getSiswa(): List<DataSiswa>
-
-    // Perhatikan: id tipe datanya Int (Bukan String)
-    suspend fun getSiswaById(id: Int): DataSiswa
-
-    suspend fun insertSiswa(siswa: DataSiswa)
-
-    suspend fun updateSiswa(id: Int, siswa: DataSiswa) // Ini juga Int
-
-    suspend fun deleteSiswa(id: Int) // Ini juga Int
+    suspend fun getDataSiswa(): List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa): Response<Void>
+    suspend fun getSatuSiswa(id: Int): DataSiswa
+    suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): Response<Void>
+    suspend fun hapusSatuSiswa(id: Int): Response<Void>
 }
 
-// 2. IMPLEMENTASI (Koki yang memasak)
-class JaringanRepositoryDataSiswa(
-    private val serviceApiSiswa: ServiceApiSiswa
-) : RepositoryDataSiswa {
-
-    override suspend fun getSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
-
-    // Ubah id: String menjadi id: Int
-    override suspend fun getSiswaById(id: Int): DataSiswa {
-        return serviceApiSiswa.getSiswaById(id)
-    }
-
-    override suspend fun insertSiswa(siswa: DataSiswa) {
-        serviceApiSiswa.insertSiswa(siswa)
-    }
-
-    // Ubah id: String menjadi id: Int
-    override suspend fun updateSiswa(id: Int, siswa: DataSiswa) {
-        serviceApiSiswa.updateSiswa(id, siswa)
-    }
-
-    // Ubah id: String menjadi id: Int
-    override suspend fun deleteSiswa(id: Int) {
-        try {
-            val response = serviceApiSiswa.deleteSiswa(id)
-            if (!response.isSuccessful) {
-                throw IOException("Gagal menghapus data. Kode: ${response.code()}")
-            }
-        } catch (e: Exception) {
-            throw e
-        }
-    }
-}
