19771531ba066838d609cb2ccc996b490037a4ea
Othallia <vianiputriocta6@gmail.com>
2025-12-26 14:37:22 +0700

    mengupdate halaman PenyediaViewModel.kt

--- app/src/main/java/com/example/questapi_032/uicontroller/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/questapi_032/uicontroller/viewmodel/provider/PenyediaViewModel.kt
@@ -1,28 +1,38 @@
-package com.example.questapi_032.uicontroller.viewmodel.provider
+package com.example.questapi_032.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.questapi_032.repositori.AplikasiDataSiswa
-import com.example.questapi_032.uicontroller.viewmodel.EntryViewModel
-import com.example.questapi_032.uicontroller.viewmodel.HomeViewModel
+import com.example.questapi_032.viewmodel.DetailViewModel
+import com.example.questapi_032.viewmodel.EditViewModel
+import com.example.questapi_032.viewmodel.EntryViewModel
+import com.example.questapi_032.viewmodel.HomeViewModel
+
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa)
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
-
-        // Initializer untuk HomeViewModel
         initializer {
             HomeViewModel(aplikasiDataSiswa().container.repositoryDataSiswa)
         }
-
-        // Initializer untuk EntryViewModel
         initializer {
             EntryViewModel(aplikasiDataSiswa().container.repositoryDataSiswa)
         }
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
     }
 }
-
-// Fungsi ekstensi untuk mengambil instance AplikasiDataSiswa
-fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa)
