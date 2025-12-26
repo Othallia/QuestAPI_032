528a9907e6f432dfc2ce13ab7dcd58052a279bbf
Othallia <vianiputriocta6@gmail.com>
2025-12-26 20:08:33 +0700

    mengupdate halaman DestinasiDetail.kt

--- app/src/main/java/com/example/questapi_032/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/questapi_032/uicontroller/route/DestinasiDetail.kt
@@ -1,30 +1,10 @@
 package com.example.questapi_032.uicontroller.route
 
-interface DestinasiNavigasi {
-    val route: String
-    val titleRes: String
-}
-
-object DestinasiHome : DestinasiNavigasi {
-    override val route = "home"
-    override val titleRes = "Home"
-}
-
-object DestinasiEntry : DestinasiNavigasi {
-    override val route = "entry"
-    override val titleRes = "Tambah Siswa"
-}
+import com.example.questapi_032.R
 
 object DestinasiDetail : DestinasiNavigasi {
-    override val route = "detail"
-    override val titleRes = "Detail Siswa"
-    const val itemIdArg = "id"
-    val routeWithArgs = "$route/{$itemIdArg}"
-}
-
-object DestinasiUpdate : DestinasiNavigasi {
-    override val route = "update"
-    override val titleRes = "Update Siswa"
-    const val itemIdArg = "id"
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
     val routeWithArgs = "$route/{$itemIdArg}"
 }
