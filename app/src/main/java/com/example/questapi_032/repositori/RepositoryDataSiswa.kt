f3e104eaad5f12b3cacd323b9fb9e9b34e2109c6
Othallia <vianiputriocta6@gmail.com>
2025-12-26 14:33:33 +0700

    memperbaiki error

--- app/src/main/java/com/example/questapi_032/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_032/repositori/RepositoryDataSiswa.kt
@@ -15,18 +15,9 @@ interface RepositoryDataSiswa {
 class JaringanRepositoryDataSiswa(
     private val serviceApiSiswa: ServiceApiSiswa
 ) : RepositoryDataSiswa {
-    override suspend fun getDataSiswa(): List<DataSiswa> =
-        serviceApiSiswa.getSiswa()
-
-    override suspend fun postDataSiswa(dataSiswa: DataSiswa): Response<Void> =
-        serviceApiSiswa.insertSiswa(dataSiswa)
-
-    override suspend fun getSatuSiswa(id: Int): DataSiswa =
-        serviceApiSiswa.getSatuSiswa(id)
-
-    override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): Response<Void> =
-        serviceApiSiswa.editSatuSiswa(id, dataSiswa)
-
-    override suspend fun hapusSatuSiswa(id: Int): Response<Void> =
-        serviceApiSiswa.hapusSatuSiswa(id)
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa): Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
+    override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): Response<Void> = serviceApiSiswa.editSatuSiswa(id, dataSiswa)
+    override suspend fun hapusSatuSiswa(id: Int): Response<Void> = serviceApiSiswa.hapusSatuSiswa(id)
 }
