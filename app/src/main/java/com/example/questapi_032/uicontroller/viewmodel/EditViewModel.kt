5abbee9488176c1786abd709b207eedce31e7ab3
Othallia <vianiputriocta6@gmail.com>
2025-12-26 14:36:44 +0700

    mengupdate halaman EditViewModel.kt

--- app/src/main/java/com/example/questapi_032/uicontroller/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_032/uicontroller/viewmodel/EditViewModel.kt
@@ -1,36 +1,69 @@
-package com.example.questapi_032.viewmodel.provider
+package com.example.questapi_032.viewmodel
 
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.createSavedStateHandle
-import androidx.lifecycle.viewmodel.CreationExtras
-import androidx.lifecycle.viewmodel.initializer
-import androidx.lifecycle.viewmodel.viewModelFactory
-import com.example.questapi_032.repositori.AplikasiDataSiswa // Sesuaikan nama class Application kamu
-import com.example.questapi_032.uicontroller.viewmodel.EntryViewModel
-import com.example.questapi_032.uicontroller.viewmodel.HomeViewModel
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi_032.modeldata.DetailSiswa
+import com.example.questapi_032.modeldata.UIStateSiswa
+import com.example.questapi_032.modeldata.toDataSiswa
+import com.example.questapi_032.modeldata.toUiStateSiswa
+import com.example.questapi_032.repositori.RepositoryDataSiswa
+import com.example.questapi_032.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
-fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa)
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDataSiswa: RepositoryDataSiswa
+) : ViewModel() {
 
-object PenyediaViewModel {
-    val Factory = viewModelFactory {
-        initializer {
-            HomeViewModel(aplikasiDataSiswa().container.repositoryDataSiswa)
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+
+    init {
+        viewModelScope.launch {
+            try {
+                val siswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                uiStateSiswa = siswa.toUiStateSiswa(isEntryValid = true)
+            } catch (e: Exception) {
+                println("Error Load Data: ${e.message}")
+            }
         }
-        initializer {
-            EntryViewModel(aplikasiDataSiswa().container.repositoryDataSiswa)
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa = UIStateSiswa(
+            detailSiswa = detailSiswa,
+            isEntryValid = validasiInput(detailSiswa)
+        )
+    }
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
         }
-        initializer {
-            DetailViewModel(
-                this.createSavedStateHandle(),
-                aplikasiDataSiswa().container.repositoryDataSiswa
-            )
-        }
-        initializer {
-            EditViewModel(
-                this.createSavedStateHandle(),
-                aplikasiDataSiswa().container.repositoryDataSiswa
-            )
+    }
+
+    suspend fun editSatuSiswa() {
+        if (validasiInput(uiStateSiswa.detailSiswa)) {
+            try {
+                val response: Response<Void> = repositoryDataSiswa.editSatuSiswa(
+                    idSiswa,
+                    uiStateSiswa.detailSiswa.toDataSiswa()
+                )
+                if (response.isSuccessful) {
+                    println("Update Sukses: ${response.message()}")
+                } else {
+                    println("Update Gagal: ${response.errorBody()}")
+                }
+            } catch (e: Exception) {
+                println("Update Error: ${e.message}")
+            }
         }
     }
 }
