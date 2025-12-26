97d209f5f9f244a382ac820c6c154e6994a81005
Othallia <vianiputriocta6@gmail.com>
2025-12-26 20:21:09 +0700

    memperbaiki error code

--- app/src/main/java/com/example/questapi_032/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/questapi_032/uicontroller/PetaNavigasi.kt
@@ -3,17 +3,22 @@ package com.example.questapi_032.uicontroller
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.questapi_032.uicontroller.route.DestinasiDetail
+import com.example.questapi_032.uicontroller.route.DestinasiEdit
 import com.example.questapi_032.uicontroller.route.DestinasiEntry
 import com.example.questapi_032.uicontroller.route.DestinasiHome
-import com.example.questapi_032.uicontroller.view.EntrySiswaScreen
 import com.example.questapi_032.uicontroller.view.HomeScreen
+import com.example.questapi_032.view.DetailSiswaScreen
+import com.example.questapi_032.view.EditSiswaScreen
+import com.example.questapi_032.view.EntrySiswaScreen
 
 @Composable
-fun DataSiswaApp(navController: NavHostController = rememberNavController(),
-                 modifier: Modifier = Modifier) {
+fun DataSiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier = Modifier) {
     HostNavigasi(navController = navController)
 }
 
@@ -27,19 +32,45 @@ fun HostNavigasi(
         startDestination = DestinasiHome.route,
         modifier = Modifier
     ) {
-        composable(DestinasiHome.route) {
+        composable(route = DestinasiHome.route) {
             HomeScreen(
+                // Ubah 'navigateToEntry' menjadi 'navigateToItemEntry'
                 navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
-                // Perbaikan: Ganti onDetailClick menjadi navigateToItemUpdate
-                navigateToItemUpdate = { id ->
-                    // Nanti kita isi bagian ini saat membuat Halaman Edit
-                    // navController.navigate("${DestinasiUpdate.route}/$id")
+
+                // Ubah 'onItemClick' atau 'onDetailClick' menjadi 'navigateToItemUpdate'
+                navigateToItemUpdate = { idSiswa ->
+                    navController.navigate("${DestinasiDetail.route}/$idSiswa")
                 }
             )
         }
         composable(DestinasiEntry.route) {
-            EntrySiswaScreen(
-                navigateBack = { navController.navigate(DestinasiHome.route) }
+            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route) })
+        }
+        composable(
+            route = DestinasiDetail.routeWithArgs,
+            arguments = listOf(navArgument(name = DestinasiDetail.itemIdArg) {
+                type = NavType.IntType
+            })
+        ) {
+            DetailSiswaScreen(
+                // Sesuaikan nama parameter menjadi navigateToEditItem
+                navigateToEditItem = { idSiswa ->
+                    navController.navigate("${DestinasiEdit.route}/$idSiswa")
+                },
+                navigateBack = {
+                    navController.navigateUp()
+                }
+            )
+        }
+        composable(
+            DestinasiEdit.routeWithArgs,
+            arguments = listOf(navArgument(DestinasiEdit.itemIdArg) {
+                type = NavType.IntType
+            })
+        ) {
+            EditSiswaScreen(
+                navigateBack = { navController.navigate(DestinasiHome.route) },
+                onNavigateUp = { navController.navigateUp() }
             )
         }
     }
