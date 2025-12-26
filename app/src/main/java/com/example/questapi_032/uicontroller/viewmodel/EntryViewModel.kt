8d6826493e11d12f2accc973962a39128545c6a1
Othallia <vianiputriocta6@gmail.com>
2025-12-26 14:35:11 +0700

    mengupdate halaman EntryViewModel.kt

--- app/src/main/java/com/example/questapi_032/uicontroller/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/questapi_032/uicontroller/viewmodel/EntryViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.questapi_032.uicontroller.viewmodel
+package com.example.questapi_032.viewmodel
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -8,37 +8,33 @@ import com.example.questapi_032.modeldata.DetailSiswa
 import com.example.questapi_032.modeldata.UIStateSiswa
 import com.example.questapi_032.modeldata.toDataSiswa
 import com.example.questapi_032.repositori.RepositoryDataSiswa
+import retrofit2.Response
 
 class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa) : ViewModel() {
-
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
 
-    /* Fungsi untuk memvalidasi input */
     private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
         return with(uiState) {
             nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
         }
     }
 
-
-    // Fungsi untuk menangani saat ada perubahan pada text input
     fun updateUiState(detailSiswa: DetailSiswa) {
-        uiStateSiswa =
-            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+        uiStateSiswa = UIStateSiswa(
+            detailSiswa = detailSiswa,
+            isEntryValid = validasiInput(detailSiswa)
+        )
     }
 
-    /* Fungsi untuk menyimpan data yang di-entry */
     suspend fun addSiswa() {
         if (validasiInput()) {
-            try {
-                // Di sini kita pakai insertSiswa sesuai yang kita buat di Repository sebelumnya
-                repositoryDataSiswa.insertSiswa(uiStateSiswa.detailSiswa.toDataSiswa())
-                println("Sukses Tambah Data")
-            } catch (e: Exception) {
-                println("Gagal tambah data : ${e.message}")
+            val response: Response<Void> = repositoryDataSiswa.postDataSiswa(uiStateSiswa.detailSiswa.toDataSiswa())
+            if (response.isSuccessful) {
+                println("Sukses Tambah Data: ${response.message()}")
+            } else {
+                println("Gagal tambah data: ${response.errorBody()}")
             }
         }
     }
 }
-
