2d650ccfe259eb37ae03c391e031090c4ed51c02
Othallia <vianiputriocta6@gmail.com>
2025-12-26 14:07:22 +0700

    menambahkan EditViewModel dengan load data siswa berdasarkan ID

--- app/src/main/java/com/example/questapi_032/uicontroller/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_032/uicontroller/viewmodel/EditViewModel.kt
@@ -1,2 +1,51 @@
 package com.example.questapi_032.uicontroller.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi_032.modeldata.DetailSiswa
+import com.example.questapi_032.modeldata.UIStateSiswa
+import com.example.questapi_032.modeldata.toDataSiswa
+import com.example.questapi_032.modeldata.toUiStateSiswa
+import com.example.questapi_032.repositori.RepositoryDataSiswa
+import com.example.questapi_032.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDataSiswa: RepositoryDataSiswa
+) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+
+    init {
+        viewModelScope.launch {
+            try {
+                val siswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                uiStateSiswa = siswa.toUiStateSiswa(isEntryValid = true)
+            } catch (e: Exception) {
+                println("Error Load Data: ${e.message}")
+            }
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa = UIStateSiswa(
+            detailSiswa = detailSiswa,
+            isEntryValid = validasiInput(detailSiswa)
+        )
+    }
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
