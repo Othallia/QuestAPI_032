85c3a83d0f9641b50c6a7070528e7c104be32f64
Othallia <vianiputriocta6@gmail.com>
2025-12-26 14:33:09 +0700

    menyesuaikan api

--- app/src/main/java/com/example/questapi_032/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_032/apiservice/ServiceApiSiswa.kt
@@ -2,36 +2,21 @@ package com.example.questapi_032.apiservice
 
 import com.example.questapi_032.modeldata.DataSiswa
 import retrofit2.Response
-import retrofit2.http.Body
-import retrofit2.http.DELETE
-import retrofit2.http.GET
-import retrofit2.http.Headers
-import retrofit2.http.POST
-import retrofit2.http.PUT
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface ServiceApiSiswa {
-    @Headers(
-        "Accept: application/json",
-        "Content-Type: application/json"
-    )
-
     @GET("bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
 
     @POST("insertTM.php")
-    suspend fun insertSiswa(@Body dataSiswa: DataSiswa): Response<Void>
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa): Response<Void>
 
-    // Menyesuaikan modul: menggunakan Query id
-    @GET("bacalTeman.php")
+    @GET("bacalTeman.php/{id}")
     suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
 
-    @PUT("editTM.php")
-    suspend fun editSatuSiswa(
-        @Query("id") id: Int,
-        @Body dataSiswa: DataSiswa
-    ): Response<Void>
+    @PUT("editTM.php/{id}")
+    suspend fun editSatuSiswa(@Query("id") id: Int, @Body dataSiswa: DataSiswa): Response<Void>
 
-    @DELETE("deleteTM.php")
+    @DELETE("deleteTM.php/{id}")
     suspend fun hapusSatuSiswa(@Query("id") id: Int): Response<Void>
 }
