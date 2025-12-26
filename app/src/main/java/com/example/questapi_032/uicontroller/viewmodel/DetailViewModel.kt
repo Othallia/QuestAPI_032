f4b39b3da638135ef1095ec81876c9953e5bb272
Othallia <vianiputriocta6@gmail.com>
2025-12-26 14:15:26 +0700

    menambahkan DetailViewModel untuk menampilkan detail data siswa

--- app/src/main/java/com/example/questapi_032/uicontroller/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_032/uicontroller/viewmodel/DetailViewModel.kt
@@ -1,2 +1,50 @@
 package com.example.questapi_032.uicontroller.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi_032.modeldata.DataSiswa
+import com.example.questapi_032.repositori.RepositoryDataSiswa
+import com.example.questapi_032.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import retrofit2.Response
+import java.io.IOException
+
+sealed interface StatusUiDetail {
+    data class Success(val satusiswa: DataSiswa) : StatusUiDetail
+    object Error : StatusUiDetail
+    object Loading : StatusUiDetail
+}
+
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDataSiswa: RepositoryDataSiswa
+) : ViewModel() {
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+
+    var statusUiDetail: StatusUiDetail by mutableStateOf(StatusUiDetail.Loading)
+        private set
+
+    init {
+        getSatuSiswa()
+    }
+
+    fun getSatuSiswa() {
+        viewModelScope.launch {
+            statusUiDetail = StatusUiDetail.Loading
+            try {
+                val satusiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                statusUiDetail = StatusUiDetail.Success(satusiswa)
+            } catch (e: IOException) {
+                statusUiDetail = StatusUiDetail.Error
+            } catch (e: HttpException) {
+                statusUiDetail = StatusUiDetail.Error
+            }
+        }
+    }
+
