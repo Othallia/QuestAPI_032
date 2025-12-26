97674b7cde4b7b33b8653eb7d2af32b342e680d9
Othallia <vianiputriocta6@gmail.com>
2025-12-26 14:05:41 +0700

    menambahkan DestinasiDetail.kt

--- app/src/main/java/com/example/questapi_032/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/questapi_032/uicontroller/route/DestinasiDetail.kt
@@ -1,2 +1,30 @@
 package com.example.questapi_032.uicontroller.route
 
+interface DestinasiNavigasi {
+    val route: String
+    val titleRes: String
+}
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home"
+}
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "entry"
+    override val titleRes = "Tambah Siswa"
+}
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail"
+    override val titleRes = "Detail Siswa"
+    const val itemIdArg = "id"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
+
+object DestinasiUpdate : DestinasiNavigasi {
+    override val route = "update"
+    override val titleRes = "Update Siswa"
+    const val itemIdArg = "id"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
